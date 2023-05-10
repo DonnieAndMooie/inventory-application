5728f973cffeb6ca2162bf4cc8b39ddbf98946fd
DonnieAndMooie <owenjeaves@gmail.com>
2023-05-10 13:06:20 +0100

    Add create console functionality

--- controllers/consoleController.js
+++ controllers/consoleController.js
@@ -1,4 +1,5 @@
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 const Console = require("../models/console");
 const Game = require("../models/game");
 
@@ -26,13 +27,40 @@ exports.console_detail = asyncHandler(async (req, res, next) => {
 });
 
 exports.console_create_get = (req, res, next) => {
-  res.send("NOT IMPLEMENTED");
+  res.render("console_form", { title: "Add Console" });
 };
 
-exports.console_create_post = (req, res, next) => {
-  res.send("NOT IMPLEMENTED");
-};
+exports.console_create_post = [
+  body("name")
+    .trim()
+    .isLength({ min: 3 })
+    .escape()
+    .withMessage("Name must be at least 3 characters long"),
+  body("release_year")
+    .optional({ checkFalsy: true })
+    .trim()
+    .escape(),
 
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const newConsole = new Console({
+      name: req.body.name,
+      release_year: req.body.release_year,
+    });
+
+    if (!errors.isEmpty()) {
+      res.render("console_form", {
+        title: "Add Console",
+        console: newConsole,
+        errors: errors.array(),
+      });
+    } else {
+      await newConsole.save();
+      res.redirect(newConsole.url);
+    }
+  }),
+];
 exports.console_delete_get = (req, res, next) => {
   res.send("NOT IMPLEMENTED");
 };
